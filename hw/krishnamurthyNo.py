78f05d8fb1226ffba52f1152ae59405b03f6ee6c
Nami <nikhitaajithg@gmail.com>
2025-05-12 11:07:20 +0000

    correct

--- hw/krishnamurthyNo.py
+++ hw/krishnamurthyNo.py
@@ -1,2 +1,18 @@
-print("hello")
-print("hello")
+n=int(input("enter the no."))
+s=n
+sum_f=0
+while s>0:
+    f=1
+    dig=s%10
+    if dig>1:
+        for i in range(2,dig+1):
+            f=f*i
+    sum_f=sum_f+f
+    s=s//10
+if sum_f==n:
+    print("krishnamurthy no")
+else:
+    print("not krishnamurthy no")
+
+
+
