f0b06cfd7fd5bcaefdc3265c2d571a6fbfa5fab6
Nami <nikhitaajithg@gmail.com>
2025-07-16 21:11:14 +0530

    Create full_pyramid.py

---
+++ full_pyramid.py
@@ -0,0 +1,12 @@
+def pyramid(n):
+    for i in range(1, n + 1):
+        # Print leading spaces
+        for j in range(n - i):
+            print(" ", end="")
+        
+        # Print asterisks for the current row
+        for k in range(1, 2*i):
+            print("*", end="")
+        print()
+   
+pyramid(5)
